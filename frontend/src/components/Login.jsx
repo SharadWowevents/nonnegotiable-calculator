175d1d4aaf1586bfd9895d316696bd5bb5145cbe
SR101234 <sharadbansal67@gmail.com>
2026-09-23 17:29:59 +0530

    add login panel

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Tracker.css';
+// import './Tracker.css';
 
 export default function Login({ setToken }) {
   const [username, setUsername] = useState('');
